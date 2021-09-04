dc0f8021bcf59af4cdc32504b6bbe74b830c45f4
Padredilg <padredilg@knights.ucf.edu>
2021-09-04 11:37:46 -0400

    answers are stored in team array, and one function call another

--- index.js
+++ index.js
@@ -7,74 +7,97 @@ const inquirer = require('inquirer');
 
 //last question for each of them must be 
     //what to add next, and it must call the next or return
+//team.name
+let team = {
+    name: '',
+    managers: [],
+    engineers: [],
+    interns: []
+};
 
-let worker;
+const addTeam = () => {
+    return inquirer.prompt([
+        {//Team's Name
+            type: 'input',
+            name: 'team',
+            message: "Please enter this team's name (required):",
+            validate: teamInput => {
+                if(teamInput){
+                    return true;
+                }
+                else{
+                    console.log("Enter this team's name!");
+                    return false;
+                }
+            }
+        }
+    ])
+};
 
-const addManager = [
-    {//Team's Name
-        type: 'input',
-        name: 'team',
-        message: "Please enter this team's name (required):",
-        validate: teamInput => {
-            if(teamInput){
-                return true;
+const addManager = () => {
+    return inquirer.prompt([
+        {//Manager's Name
+            type: 'input',
+            name: 'manager',
+            message: "Please enter the manager's name (required):",
+            validate: managerInput => {
+                if(managerInput){
+                    worker = managerInput;
+                    return true;
+                }
+                else{
+                    console.log("Enter the manager's name!");
+                    return false;
+                }
             }
-            else{
-                console.log("Enter this team's name!");
-                return false;
+        },
+        {//Manager's ID
+            type: 'input',
+            name: 'managerId',
+            message: "Please enter the manager's ID (required):",
+            validate: idInput => {
+                if(idInput){
+                    return true;
+                }
+                else{
+                    console.log("Enter the manager's ID!");
+                    return false;
+                }
             }
+        },
+        {//Manager's Office Number
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the manager's Office Number (required):",
+            validate: officeNumberInput => {
+                if(officeNumberInput){
+                    return true;
+                }
+                else{
+                    console.log("Enter the manager's Office Number!");
+                    return false;
+                }
+            }
+        },
+        {//What's Next?
+            type: 'list',
+            name: 'next',
+            message: "What would you like to do next?",
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish Team']
         }
-    },
-    {//Manager's Name
-        type: 'input',
-        name: 'manager',
-        message: "Please enter the manager's name (required):",
-        validate: managerInput => {
-            if(managerInput){
-                worker = managerInput;
-                return true;
-            }
-            else{
-                console.log("Enter the manager's name!");
-                return false;
-            }
+    ])
+    .then(managerInfo => {
+        team.managers.push(managerInfo)
+        //check what managerInfo.choices is and act accordingly
+        if(managerInfo.choices === 'Add an Engineer'){
+            addEngineer();
         }
-    },
-    {//Manager's ID
-        type: 'input',
-        name: 'id',
-        message: "Please enter the manager's ID (required):",
-        validate: idInput => {
-            if(idInput){
-                return true;
-            }
-            else{
-                console.log("Enter the manager's ID!");
-                return false;
-            }
+        else if(managerInfo.choices === 'Add an Intern'){
+            addIntern();
         }
-    },
-    {//Manager's Office Number
-        type: 'input',
-        name: 'officeNumber',
-        message: "Please enter the manager's Office Number (required):",
-        validate: officeNumberInput => {
-            if(officeNumberInput){
-                return true;
-            }
-            else{
-                console.log("Enter the manager's Office Number!");
-                return false;
-            }
-        }
-    },
-    {//What's Next?
-        type: 'list',
-        name: 'next',
-        message: "What would you like to do next?",
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish Team']
-    }
-];
+    })
+}
+
 
 const addEngineer = [];
 
@@ -82,9 +105,10 @@ const addIntern = [];
 
 //Call questions and store answers in array
 function init() {
-    inquirer.prompt(addManager)
+    addTeam()
         .then(teamData => {
-            console.log(teamData);
+            team.name = teamData.team;
+            addManager().then(teamInfo => console.log(team));
         })
 }
 
@@ -97,3 +121,6 @@ init();
 //Create CSS in dist
 
 //write file to dist
+
+//I want to have an answers array that will have
+//team.name, team.manager, team.engineers, team.interns
