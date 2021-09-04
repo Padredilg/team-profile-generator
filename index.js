8e9928beaf05e9442cf820520491694edff5ec71
Padredilg <padredilg@knights.ucf.edu>
2021-09-04 08:45:51 -0400

    correctly installed all packages and added prompts to collect managers info

--- index.js
+++ index.js
@@ -1,32 +1,99 @@
-/*
-AS A manager
+//require packages and files
+const inquirer = require('inquirer');
 
-I WANT to generate a webpage that displays my team's basic info
-SO THAT I have quick access to their emails and GitHub profiles
+//Create questions
+//One array for each employee type
+//Manager is the first to be called
 
-GIVEN a command-line application that accepts user input
+//last question for each of them must be 
+    //what to add next, and it must call the next or return
 
-WHEN I am prompted for my team members and their information
-THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+let worker;
 
-WHEN I click on an email address in the HTML
-THEN my default email program opens and populates the TO field of the email with the address
+const addManager = [
+    {//Team's Name
+        type: 'input',
+        name: 'team',
+        message: "Please enter this team's name (required):",
+        validate: teamInput => {
+            if(teamInput){
+                return true;
+            }
+            else{
+                console.log("Enter this team's name!");
+                return false;
+            }
+        }
+    },
+    {//Manager's Name
+        type: 'input',
+        name: 'manager',
+        message: "Please enter the manager's name (required):",
+        validate: managerInput => {
+            if(managerInput){
+                worker = managerInput;
+                return true;
+            }
+            else{
+                console.log("Enter the manager's name!");
+                return false;
+            }
+        }
+    },
+    {//Manager's ID
+        type: 'input',
+        name: 'id',
+        message: "Please enter the manager's ID (required):",
+        validate: idInput => {
+            if(idInput){
+                return true;
+            }
+            else{
+                console.log("Enter the manager's ID!");
+                return false;
+            }
+        }
+    },
+    {//Manager's Office Number
+        type: 'input',
+        name: 'officeNumber',
+        message: "Please enter the manager's Office Number (required):",
+        validate: officeNumberInput => {
+            if(officeNumberInput){
+                return true;
+            }
+            else{
+                console.log("Enter the manager's Office Number!");
+                return false;
+            }
+        }
+    },
+    {//What's Next?
+        type: 'list',
+        name: 'next',
+        message: "What would you like to do next?",
+        choices: ['Add an Engineer', 'Add an Intern', 'Finish Team']
+    }
+];
 
-WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
+const addEngineer = [];
 
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+const addIntern = [];
 
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+//Call questions and store answers in array
+function init() {
+    inquirer.prompt(addManager)
+        .then(teamData => {
+            console.log(teamData);
+        })
+}
 
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+init();
 
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+//Create all classes and their tests - ?
 
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated
-*/
+//Create template literal in src
+
+//Create CSS in dist
+
+//write file to dist
